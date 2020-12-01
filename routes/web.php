5559f87353f7dcac971f09751b1144b1427848de
Oskar Šerelis <oskaras34@gmail.com>
2020-12-01 17:14:52 +0200

    trying to fix deployment errors

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::get('/', function () {
-//     // return view('layouts/welcome');
-//     return view('main');
-// });
+Route::get('/', function () {
+    // return view('layouts/welcome');
+    return view('main');
+});
 
 Auth::routes();
 
