479c3894b61fdd22830d0cc4dd6ae24049cc8c5e
Oskar Šerelis <oskaras34@gmail.com>
2020-12-01 20:16:28 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    // return view('layouts/welcome');
-    return view('main');
-});
+Route::view('/', 'main');
 // 
 Auth::routes();
 
